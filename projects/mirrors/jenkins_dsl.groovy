ae5f03dea6fd6542a7b408f27e9159e3afb2bd82
Jess Frazelle <acidburn@google.com>
2017-01-15 20:43:05 -0800

    add jenkins-dsl
    Signed-off-by: Jess Frazelle <acidburn@google.com>

---
+++ projects/mirrors/jenkins_dsl.groovy
@@ -0,0 +1,66 @@
+freeStyleJob('mirror_jenkins_dsl') {
+    displayName('mirror-jenkins-dsl')
+    description('Mirror github.com/jessfraz/jenkins-dsl to g.j3ss.co/jenkins-dsl.')
+
+    checkoutRetryCount(3)
+
+    properties {
+        githubProjectUrl('https://github.com/jessfraz/jenkins-dsl')
+        sidebarLinks {
+            link('https://git.j3ss.co/jenkins-dsl', 'git.j3ss.co/jenkins-dsl', 'notepad.png')
+        }
+    }
+
+    logRotator {
+        numToKeep(2)
+        daysToKeep(2)
+    }
+
+    scm {
+        git {
+            remote {
+                url('git@github.com:jessfraz/jenkins-dsl.git')
+                name('origin')
+                credentials('ssh-github-key')
+                refspec('+refs/heads/master:refs/remotes/origin/master')
+            }
+            remote {
+                url('ssh://git@g.j3ss.co:2200/~/jenkins-dsl.git')
+                name('mirror')
+                credentials('ssh-github-key')
+                refspec('+refs/heads/master:refs/remotes/upstream/master')
+            }
+            branches('master')
+            extensions {
+                wipeOutWorkspace()
+                cleanAfterCheckout()
+            }
+        }
+    }
+
+    triggers {
+        cron('H H * * *')
+    }
+
+    wrappers { colorizeOutput() }
+
+    publishers {
+        postBuildScripts {
+            git {
+                branch('mirror', 'master')
+            }
+        }
+
+        extendedEmail {
+            recipientList('$DEFAULT_RECIPIENTS')
+            contentType('text/plain')
+            triggers {
+                stillFailing {
+                    attachBuildLog(true)
+                }
+            }
+        }
+
+        wsCleanup()
+    }
+}

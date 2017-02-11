b36c3d8ece0c9703b8fba9a41dc4fb1fbda51fde
Jess Frazelle <acidburn@google.com>
2017-02-11 15:18:52 -0800

    fix broken alpine latest with DCT
    Signed-off-by: Jess Frazelle <acidburn@google.com>

--- projects/repo-dockerfiles/s3server.groovy
+++ projects/repo-dockerfiles/s3server.groovy
@@ -36,7 +36,7 @@ branches('*/master')
 
     wrappers { colorizeOutput() }
 
-    environmentVariables(DOCKER_CONTENT_TRUST: '1')
+    environmentVariables(DOCKER_CONTENT_TRUST: '0')
     steps {
         shell('docker build --rm --force-rm -t r.j3ss.co/s3server:latest .')
         shell('docker tag r.j3ss.co/s3server:latest jess/s3server:latest')

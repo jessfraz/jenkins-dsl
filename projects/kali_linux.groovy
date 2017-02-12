bfc0457894ea9224547def9eb6e498dbc92b334a
Jess Frazelle <acidburn@google.com>
2017-02-12 13:54:57 -0800

    fix
    Signed-off-by: Jess Frazelle <acidburn@google.com>

--- projects/kali_linux.groovy
+++ projects/kali_linux.groovy
@@ -38,7 +38,7 @@ freeStyleJob('kali_linux') {
         shell('docker run --rm --privileged -v $(which docker):/usr/bin/docker:ro -v $(pwd | sed \'s#/var/jenkins_home/#/var/lib/docker/jenkins/#\')/build.sh:/usr/bin/build.sh:ro -v /var/run/docker.sock:/var/run/docker.sock debian build.sh')
         shell('docker push --disable-content-trust=false r.j3ss.co/kalilinux:latest')
 		shell('docker tag r.j3ss.co/kalilinux:latest jess/kalilinux:latest')
-        shell('docker push --disable-content-trust=false jess/kalilinux:latest')
+        shell('docker push --disable-content-trust=true jess/kalilinux:latest')
         shell('docker run --rm --disable-content-trust=true r.j3ss.co/kalilinux awk \'{print $NF}\' /etc/debian_version | sed \'s/\\r$//\' | tr \'[:upper:]\' \'[:lower:]\' > tag')
 		shell('docker tag r.j3ss.co/kalilinux:latest r.j3ss.co/kalilinux:$(cat tag)')
         shell('docker push --disable-content-trust=false r.j3ss.co/kalilinux:$(cat tag)')

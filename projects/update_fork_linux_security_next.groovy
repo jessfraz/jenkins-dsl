74281938039ea890d79eebebcaa8ce4eeb020dae
Jess Frazelle <acidburn@google.com>
2017-02-12 12:42:33 -0800

    fix
    Signed-off-by: Jess Frazelle <acidburn@google.com>

--- projects/update_fork_linux_security_next.groovy
+++ projects/update_fork_linux_security_next.groovy
@@ -28,6 +28,7 @@ freeStyleJob('update_fork_linux_security_next') {
             }
             branches('linux-security-next', 'upstream/next')
             extensions {
+                cloneOptions {
                     timeout(20)
                 }
                 wipeOutWorkspace()

55085deb12b8085bb9939a8a5a181bde013639fc
Jess Frazelle <acidburn@google.com>
2017-05-30 14:48:14 -0400

    update
    Signed-off-by: Jess Frazelle <acidburn@google.com>

---
+++ projects/forks/syzkaller.groovy
@@ -0,0 +1,69 @@
+freeStyleJob('update_fork_syzkaller') {
+    displayName('update-fork-syzkaller')
+    description('Rebase the primary branch (master) in jessfraz/syzkaller fork.')
+
+    checkoutRetryCount(3)
+
+    properties {
+        githubProjectUrl('https://github.com/jessfraz/syzkaller')
+        sidebarLinks {
+            link('https://github.com/google/syzkaller', 'UPSTREAM: google/syzkaller', 'notepad.png')
+        }
+    }
+
+    logRotator {
+        numToKeep(2)
+        daysToKeep(2)
+    }
+
+    scm {
+        git {
+            remote {
+                url('git@github.com:jessfraz/syzkaller.git')
+                name('origin')
+                credentials('ssh-github-key')
+                refspec('+refs/heads/master:refs/remotes/origin/master')
+            }
+            remote {
+                url('https://github.com/google/syzkaller.git')
+                name('upstream')
+                refspec('+refs/heads/master:refs/remotes/upstream/master')
+            }
+            branches('master', 'upstream/master')
+            extensions {
+                disableRemotePoll()
+                wipeOutWorkspace()
+                cleanAfterCheckout()
+            }
+        }
+    }
+
+    triggers {
+        cron('H H * * *')
+    }
+
+    wrappers { colorizeOutput() }
+
+    steps {
+        shell('git rebase upstream/master')
+    }
+
+    publishers {
+        git {
+            branch('origin', 'master')
+            pushOnlyIfSuccess()
+        }
+
+        extendedEmail {
+            recipientList('$DEFAULT_RECIPIENTS')
+            contentType('text/plain')
+            triggers {
+                stillFailing {
+                    attachBuildLog(true)
+                }
+            }
+        }
+
+        wsCleanup()
+    }
+}

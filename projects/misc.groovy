99bc2d84d236f7702861af84ff06559e635e5f73
Jess Frazelle <acidburn@google.com>
2017-07-06 05:01:03 -0400

    add misc
    Signed-off-by: Jess Frazelle <acidburn@google.com>

---
+++ projects/misc.groovy
@@ -0,0 +1,61 @@
+freeStyleJob('misc') {
+    displayName('misc')
+    description('Run linters and tests for misc repository.')
+
+    checkoutRetryCount(3)
+
+    properties {
+        githubProjectUrl('https://github.com/jessfraz/misc')
+    }
+
+    logRotator {
+        numToKeep(2)
+        daysToKeep(2)
+    }
+
+    scm {
+        git {
+            remote {
+                url('git@github.com:jessfraz/misc.git')
+                credentials('misc-deploy-key')
+            }
+            branches('*/master')
+            extensions {
+                wipeOutWorkspace()
+                cleanAfterCheckout()
+            }
+        }
+    }
+
+    triggers {
+        cron('H H * * *')
+        githubPush()
+    }
+
+    wrappers { colorizeOutput() }
+
+    environmentVariables(DOCKER_CONTENT_TRUST: '1')
+    steps {
+        shell('if [ ! -f /usr/bin/make ] ; then docker exec -u root jenkins apk add --no-cache make; fi')
+        shell('make')
+    }
+
+    publishers {
+        retryBuild {
+            retryLimit(2)
+            fixedDelay(15)
+        }
+
+        extendedEmail {
+            recipientList('$DEFAULT_RECIPIENTS')
+            contentType('text/plain')
+            triggers {
+                stillFailing {
+                    attachBuildLog(true)
+                }
+            }
+        }
+
+        wsCleanup()
+    }
+}

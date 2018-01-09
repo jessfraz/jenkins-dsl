9b64d4daba8f577348d6adcb1d54ff77fe389bf6
Jess Frazelle <acidburn@microsoft.com>
2018-01-09 13:40:27 -0500

    add sig-release
    Signed-off-by: Jess Frazelle <acidburn@microsoft.com>

---
+++ projects/forks/sig_release.groovy
@@ -0,0 +1,69 @@
+freeStyleJob('update_fork_sig_release') {
+    displayName('update-fork-sig-release')
+    description('Rebase the primary branch (master) in jessfraz/sig-release fork.')
+
+    checkoutRetryCount(3)
+
+    properties {
+        githubProjectUrl('https://github.com/jessfraz/sig-release')
+        sidebarLinks {
+            link('https://github.com/kubernetes/sig-release', 'UPSTREAM: kubernetes/sig-release', 'notepad.png')
+        }
+    }
+
+    logRotator {
+        numToKeep(100)
+        daysToKeep(15)
+    }
+
+    scm {
+        git {
+            remote {
+                url('git@github.com:jessfraz/sig-release.git')
+                name('origin')
+                credentials('ssh-github-key')
+                refspec('+refs/heads/master:refs/remotes/origin/master')
+            }
+            remote {
+                url('https://github.com/kubernetes/sig-release.git')
+                name('upstream')
+                refspec('+refs/heads/master:refs/remotes/upstream/master')
+            }
+            branches('master', 'upstream/master')
+            extensions {
+                disableRemotePoll()
+                wipeOutWorkspace()
+                cleanAfterCheckout()
+            }
+        }
+    }
+
+    triggers {
+        cron('H H * * *')
+    }
+
+    wrappers { colorizeOutput() }
+
+    steps {
+        shell('git rebase upstream/master')
+    }
+
+    publishers {
+        git {
+            branch('origin', 'master')
+            pushOnlyIfSuccess()
+        }
+
+        extendedEmail {
+            recipientList('$DEFAULT_RECIPIENTS')
+            contentType('text/plain')
+            triggers {
+                stillFailing {
+                    attachBuildLog(true)
+                }
+            }
+        }
+
+        wsCleanup()
+    }
+}

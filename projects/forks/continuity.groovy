4621e75bbb4f92a1cfae8761102363c3f1dce11c
Jess Frazelle <acidburn@microsoft.com>
2018-02-20 01:50:34 -0500

    add continuity
    Signed-off-by: Jess Frazelle <acidburn@microsoft.com>

---
+++ projects/forks/continuity.groovy
@@ -0,0 +1,69 @@
+freeStyleJob('update_fork_continuity') {
+    displayName('update-fork-continuity')
+    description('Rebase the primary branch (master) in jessfraz/continuity fork.')
+
+    checkoutRetryCount(3)
+
+    properties {
+        githubProjectUrl('https://github.com/jessfraz/continuity')
+        sidebarLinks {
+            link('https://github.com/containerd/continuity', 'UPSTREAM: containerd/continuity', 'notepad.png')
+        }
+    }
+
+    logRotator {
+        numToKeep(100)
+        daysToKeep(15)
+    }
+
+    scm {
+        git {
+            remote {
+                url('git@github.com:jessfraz/continuity.git')
+                name('origin')
+                credentials('ssh-github-key')
+                refspec('+refs/heads/master:refs/remotes/origin/master')
+            }
+            remote {
+                url('https://github.com/containerd/continuity.git')
+                name('upstream')
+                refspec('+refs/heads/master:refs/remotes/upstream/master')
+            }
+            branches('master', 'upstream/master')
+            extensions {
+                disableRemotePoll()
+                wipeOutWorkspace()
+                cleanAfterCheckout()
+            }
+        }
+    }
+
+    triggers {
+        cron('H H * * *')
+    }
+
+    wrappers { colorizeOutput() }
+
+    steps {
+        shell('git rebase upstream/master')
+    }
+
+    publishers {
+        git {
+            branch('origin', 'master')
+            pushOnlyIfSuccess()
+        }
+
+        extendedEmail {
+            recipientList('$DEFAULT_RECIPIENTS')
+            contentType('text/plain')
+            triggers {
+                stillFailing {
+                    attachBuildLog(true)
+                }
+            }
+        }
+
+        wsCleanup()
+    }
+}

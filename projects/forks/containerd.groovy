c3440b0444a5585fae1741efb008276c75fc0fb1
Jess Frazelle <acidburn@microsoft.com>
2018-02-20 00:37:30 -0500

    update forks
    Signed-off-by: Jess Frazelle <acidburn@microsoft.com>

---
+++ projects/forks/containerd.groovy
@@ -0,0 +1,69 @@
+freeStyleJob('update_fork_containerd') {
+    displayName('update-fork-containerd')
+    description('Rebase the primary branch (master) in jessfraz/containerd fork.')
+
+    checkoutRetryCount(3)
+
+    properties {
+        githubProjectUrl('https://github.com/jessfraz/containerd')
+        sidebarLinks {
+            link('https://github.com/containerd/containerd', 'UPSTREAM: containerd/containerd', 'notepad.png')
+        }
+    }
+
+    logRotator {
+        numToKeep(100)
+        daysToKeep(15)
+    }
+
+    scm {
+        git {
+            remote {
+                url('git@github.com:jessfraz/containerd.git')
+                name('origin')
+                credentials('ssh-github-key')
+                refspec('+refs/heads/master:refs/remotes/origin/master')
+            }
+            remote {
+                url('https://github.com/containerd/containerd.git')
+                name('upstream')
+                refspec('+refs/heads/master:refs/remotes/upstream/master')
+            }
+            branches('master', 'upstream/master')
+            extensions {
+                disableRemotePoll()
+                wipeOutWorkspace()
+                cleanAfterCheckout()
+            }
+        }
+    }
+
+    triggers {
+        cron('H H * * *')
+    }
+
+    wrappers { colorizeOutput() }
+
+    steps {
+        shell('git rebase upstream/master')
+    }
+
+    publishers {
+        git {
+            branch('origin', 'master')
+            pushOnlyIfSuccess()
+        }
+
+        extendedEmail {
+            recipientList('$DEFAULT_RECIPIENTS')
+            contentType('text/plain')
+            triggers {
+                stillFailing {
+                    attachBuildLog(true)
+                }
+            }
+        }
+
+        wsCleanup()
+    }
+}

a5188328b89eaafa2d53c97c40f8699545ff1cb7
Jess Frazelle <acidburn@microsoft.com>
2018-03-02 13:17:51 -0500

    add fork
    Signed-off-by: Jess Frazelle <acidburn@microsoft.com>

---
+++ projects/forks/go_runc.groovy
@@ -0,0 +1,69 @@
+freeStyleJob('update_fork_go_runc') {
+    displayName('update-fork-go-runc')
+    description('Rebase the primary branch (master) in jessfraz/go-runc fork.')
+
+    checkoutRetryCount(3)
+
+    properties {
+        githubProjectUrl('https://github.com/jessfraz/go-runc')
+        sidebarLinks {
+            link('https://github.com/containerd/go-runc', 'UPSTREAM: containerd/go-runc', 'notepad.png')
+        }
+    }
+
+    logRotator {
+        numToKeep(100)
+        daysToKeep(15)
+    }
+
+    scm {
+        git {
+            remote {
+                url('git@github.com:jessfraz/go-runc.git')
+                name('origin')
+                credentials('ssh-github-key')
+                refspec('+refs/heads/master:refs/remotes/origin/master')
+            }
+            remote {
+                url('https://github.com/containerd/go-runc.git')
+                name('upstream')
+                refspec('+refs/heads/master:refs/remotes/upstream/master')
+            }
+            branches('master', 'upstream/master')
+            extensions {
+                disableRemotePoll()
+                wipeOutWorkspace()
+                cleanAfterCheckout()
+            }
+        }
+    }
+
+    triggers {
+        cron('H H * * *')
+    }
+
+    wrappers { colorizeOutput() }
+
+    steps {
+        shell('git rebase upstream/master')
+    }
+
+    publishers {
+        git {
+            branch('origin', 'master')
+            pushOnlyIfSuccess()
+        }
+
+        extendedEmail {
+            recipientList('$DEFAULT_RECIPIENTS')
+            contentType('text/plain')
+            triggers {
+                stillFailing {
+                    attachBuildLog(true)
+                }
+            }
+        }
+
+        wsCleanup()
+    }
+}

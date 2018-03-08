d5627713aec819b5e765a7d4f2699fe91ba66eac
Jess Frazelle <acidburn@microsoft.com>
2018-03-08 15:38:40 -0500

    update forks
    Signed-off-by: Jess Frazelle <acidburn@microsoft.com>

---
+++ projects/forks/cgroups.groovy
@@ -0,0 +1,69 @@
+freeStyleJob('update_fork_cgroups') {
+    displayName('update-fork-cgroups')
+    description('Rebase the primary branch (master) in jessfraz/cgroups fork.')
+
+    checkoutRetryCount(3)
+
+    properties {
+        githubProjectUrl('https://github.com/jessfraz/cgroups')
+        sidebarLinks {
+            link('https://github.com/containerd/cgroups', 'UPSTREAM: containerd/cgroups', 'notepad.png')
+        }
+    }
+
+    logRotator {
+        numToKeep(100)
+        daysToKeep(15)
+    }
+
+    scm {
+        git {
+            remote {
+                url('git@github.com:jessfraz/cgroups.git')
+                name('origin')
+                credentials('ssh-github-key')
+                refspec('+refs/heads/master:refs/remotes/origin/master')
+            }
+            remote {
+                url('https://github.com/containerd/cgroups.git')
+                name('upstream')
+                refspec('+refs/heads/master:refs/remotes/upstream/master')
+            }
+            branches('master', 'upstream/master')
+            extensions {
+                disableRemotePoll()
+                wipeOutWorkspace()
+                cleanAfterCheckout()
+            }
+        }
+    }
+
+    triggers {
+        cron('H H * * *')
+    }
+
+    wrappers { colorizeOutput() }
+
+    steps {
+        shell('git rebase upstream/master')
+    }
+
+    publishers {
+        git {
+            branch('origin', 'master')
+            pushOnlyIfSuccess()
+        }
+
+        extendedEmail {
+            recipientList('$DEFAULT_RECIPIENTS')
+            contentType('text/plain')
+            triggers {
+                stillFailing {
+                    attachBuildLog(true)
+                }
+            }
+        }
+
+        wsCleanup()
+    }
+}

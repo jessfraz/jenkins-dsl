90ef9e48e474b6ea61e887d0fa32549aea57205f
Jess Frazelle <acidburn@microsoft.com>
2018-03-30 11:34:31 -0400

    add paws
    Signed-off-by: Jess Frazelle <acidburn@microsoft.com>

---
+++ projects/paws.groovy
@@ -0,0 +1,63 @@
+freeStyleJob('paws') {
+    displayName('paws')
+    description('Build Dockerfiles for paws.')
+
+    checkoutRetryCount(3)
+
+    properties {
+        githubProjectUrl('https://github.com/jessfraz/paws')
+    }
+
+    logRotator {
+        numToKeep(100)
+        daysToKeep(15)
+    }
+
+    scm {
+        git {
+            remote {
+                url('git@github.com:jessfraz/paws.git')
+                credentials('paws-deploy-key')
+            }
+            branches('*/master')
+            extensions {
+                wipeOutWorkspace()
+                cleanAfterCheckout()
+            }
+        }
+    }
+
+    triggers {
+        cron('H H * * *')
+        githubPush()
+    }
+
+    wrappers { colorizeOutput() }
+
+    environmentVariables(DOCKER_CONTENT_TRUST: '1')
+    steps {
+        shell('make build')
+        shell('docker push --disable-content-trust=false r.j3ss.co/paws:latest')
+        shell('docker rm $(docker ps --filter status=exited -q 2>/dev/null) 2> /dev/null || true')
+        shell('docker rmi $(docker images --filter dangling=true -q 2>/dev/null) 2> /dev/null || true')
+    }
+
+    publishers {
+        retryBuild {
+            retryLimit(2)
+            fixedDelay(15)
+        }
+
+        extendedEmail {
+            recipientList('$DEFAULT_RECIPIENTS')
+            contentType('text/plain')
+            triggers {
+                stillFailing {
+                    attachBuildLog(true)
+                }
+            }
+        }
+
+        wsCleanup()
+    }
+}

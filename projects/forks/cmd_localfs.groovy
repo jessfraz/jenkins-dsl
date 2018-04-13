ff7298ab2fb6a0ac4500c98a53afab77d5f428ef
Jess Frazelle <acidburn@microsoft.com>
2018-04-13 03:12:46 -0400

    update forks
    Signed-off-by: Jess Frazelle <acidburn@microsoft.com>

---
+++ projects/forks/cmd_localfs.groovy
@@ -0,0 +1,69 @@
+freeStyleJob('update_fork_cmd_localfs') {
+    displayName('update-fork-cmd-localfs')
+    description('Rebase the primary branch (master) in jessfraz/cmd-localfs fork.')
+
+    checkoutRetryCount(3)
+
+    properties {
+        githubProjectUrl('https://github.com/jessfraz/cmd-localfs')
+        sidebarLinks {
+            link('https://github.com/gl-prototypes/cmd-localfs', 'UPSTREAM: gl-prototypes/cmd-localfs', 'notepad.png')
+        }
+    }
+
+    logRotator {
+        numToKeep(100)
+        daysToKeep(15)
+    }
+
+    scm {
+        git {
+            remote {
+                url('git@github.com:jessfraz/cmd-localfs.git')
+                name('origin')
+                credentials('ssh-github-key')
+                refspec('+refs/heads/master:refs/remotes/origin/master')
+            }
+            remote {
+                url('https://github.com/gl-prototypes/cmd-localfs.git')
+                name('upstream')
+                refspec('+refs/heads/master:refs/remotes/upstream/master')
+            }
+            branches('master', 'upstream/master')
+            extensions {
+                disableRemotePoll()
+                wipeOutWorkspace()
+                cleanAfterCheckout()
+            }
+        }
+    }
+
+    triggers {
+        cron('H H * * *')
+    }
+
+    wrappers { colorizeOutput() }
+
+    steps {
+        shell('git rebase upstream/master')
+    }
+
+    publishers {
+        git {
+            branch('origin', 'master')
+            pushOnlyIfSuccess()
+        }
+
+        extendedEmail {
+            recipientList('$DEFAULT_RECIPIENTS')
+            contentType('text/plain')
+            triggers {
+                stillFailing {
+                    attachBuildLog(true)
+                }
+            }
+        }
+
+        wsCleanup()
+    }
+}

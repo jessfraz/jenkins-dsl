a7aecb740e537bfb1520d329ac641604df991b12
Jess Frazelle <acidburn@microsoft.com>
2018-05-31 19:09:57 -0400

    update forks
    Signed-off-by: Jess Frazelle <acidburn@microsoft.com>

---
+++ projects/forks/bolt.groovy
@@ -0,0 +1,69 @@
+freeStyleJob('update_fork_bolt') {
+    displayName('update-fork-bolt')
+    description('Rebase the primary branch (master) in jessfraz/bolt fork.')
+
+    checkoutRetryCount(3)
+
+    properties {
+        githubProjectUrl('https://github.com/jessfraz/bolt')
+        sidebarLinks {
+            link('https://github.com/boltdb/bolt', 'UPSTREAM: boltdb/bolt', 'notepad.png')
+        }
+    }
+
+    logRotator {
+        numToKeep(100)
+        daysToKeep(15)
+    }
+
+    scm {
+        git {
+            remote {
+                url('git@github.com:jessfraz/bolt.git')
+                name('origin')
+                credentials('ssh-github-key')
+                refspec('+refs/heads/master:refs/remotes/origin/master')
+            }
+            remote {
+                url('https://github.com/boltdb/bolt.git')
+                name('upstream')
+                refspec('+refs/heads/master:refs/remotes/upstream/master')
+            }
+            branches('master', 'upstream/master')
+            extensions {
+                disableRemotePoll()
+                wipeOutWorkspace()
+                cleanAfterCheckout()
+            }
+        }
+    }
+
+    triggers {
+        cron('H H * * *')
+    }
+
+    wrappers { colorizeOutput() }
+
+    steps {
+        shell('git rebase upstream/master')
+    }
+
+    publishers {
+        git {
+            branch('origin', 'master')
+            pushOnlyIfSuccess()
+        }
+
+        extendedEmail {
+            recipientList('$DEFAULT_RECIPIENTS')
+            contentType('text/plain')
+            triggers {
+                stillFailing {
+                    attachBuildLog(true)
+                }
+            }
+        }
+
+        wsCleanup()
+    }
+}

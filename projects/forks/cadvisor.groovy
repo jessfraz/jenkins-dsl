1ef6f3655f7d429948525d74d87f567d55c16427
Jess Frazelle <acidburn@microsoft.com>
2018-06-14 18:00:18 -0400

    update forks
    Signed-off-by: Jess Frazelle <acidburn@microsoft.com>

---
+++ projects/forks/cadvisor.groovy
@@ -0,0 +1,69 @@
+freeStyleJob('update_fork_cadvisor') {
+    displayName('update-fork-cadvisor')
+    description('Rebase the primary branch (master) in jessfraz/cadvisor fork.')
+
+    checkoutRetryCount(3)
+
+    properties {
+        githubProjectUrl('https://github.com/jessfraz/cadvisor')
+        sidebarLinks {
+            link('https://github.com/google/cadvisor', 'UPSTREAM: google/cadvisor', 'notepad.png')
+        }
+    }
+
+    logRotator {
+        numToKeep(100)
+        daysToKeep(15)
+    }
+
+    scm {
+        git {
+            remote {
+                url('git@github.com:jessfraz/cadvisor.git')
+                name('origin')
+                credentials('ssh-github-key')
+                refspec('+refs/heads/master:refs/remotes/origin/master')
+            }
+            remote {
+                url('https://github.com/google/cadvisor.git')
+                name('upstream')
+                refspec('+refs/heads/master:refs/remotes/upstream/master')
+            }
+            branches('master', 'upstream/master')
+            extensions {
+                disableRemotePoll()
+                wipeOutWorkspace()
+                cleanAfterCheckout()
+            }
+        }
+    }
+
+    triggers {
+        cron('H H * * *')
+    }
+
+    wrappers { colorizeOutput() }
+
+    steps {
+        shell('git rebase upstream/master')
+    }
+
+    publishers {
+        git {
+            branch('origin', 'master')
+            pushOnlyIfSuccess()
+        }
+
+        extendedEmail {
+            recipientList('$DEFAULT_RECIPIENTS')
+            contentType('text/plain')
+            triggers {
+                stillFailing {
+                    attachBuildLog(true)
+                }
+            }
+        }
+
+        wsCleanup()
+    }
+}
